a8426016ec2f0848c5f053319d022bd66d93fae2
vishalicious213 <vishalicious213@gmail.com>
2023-06-19 16:37:18 -0700

    basic layout for header

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function Header() {
     return (
         <header>
             <nav>
-                <img src="./react-logo.png" />
-                <ul>
+                <img className="nav-logo" src="./react-logo.png" />
+                <ul className="nav-items">
                     <li>Pricing</li>
                     <li>About</li>
                     <li>Contact</li>
