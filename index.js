3fcd0916ddcdb1c2c08373d70395fcaadf17b284
vishalicious213 <vishalicious213@gmail.com>
2023-06-19 13:44:08 -0700

    updated index.js with Section and rendered it

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+const Section = (
+    <section>
+        <img src="./react-logo.png" />
+    
+        <h1>Fun facts about React</h1>
+
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </section>
+)
+
+ReactDOM.render(Section, document.getElementById("root"))
