0daca0000154cbc1d5a12c5e214b7f718363475e
vishalicious213 <vishalicious213@gmail.com>
2023-06-19 14:05:54 -0700

    updated index.js with Section v3 and rendered it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// VERSION 2
+// VERSION 3
 
-function section() {
+function Section() {
     return (
         <section>
             <img src="./react-logo.png" />
@@ -18,7 +18,30 @@ function section() {
     )
 }
 
-ReactDOM.render(section(), document.getElementById("root"))
+ReactDOM.render(<Section />, document.getElementById("root"))
+
+
+// VERSION 2
+
+// function section() {
+//     return (
+//         <section>
+//             <img src="./react-logo.png" />
+        
+//             <h1>Fun facts about React</h1>
+    
+//             <ul>
+//                 <li>Was first released in 2013</li>
+//                 <li>Was originally created by Jordan Walke</li>
+//                 <li>Has well over 100k stars on Github</li>
+//                 <li>Is maintained by Facebook</li>
+//                 <li>Powers thousands of enterprise apps, including mobile apps</li>
+//             </ul>
+//         </section>
+//     )
+// }
+
+// ReactDOM.render(section(), document.getElementById("root"))
 
 
 // VERSION 1
